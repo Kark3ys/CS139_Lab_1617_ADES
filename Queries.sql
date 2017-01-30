231cca5736f6ab3371df29e07a326e2250e4eb68
elliotsymons13 <elliotsymons13@gmail.com>
2017-01-30 18:41:34 +0000

    Added test Queries to Queries.sql

---
+++ Queries.sql
@@ -0,0 +1,10 @@
+SELECT * FROM lists WHERE
+listID=listRel.listID AND
+listRel.userID=<input_user_ID_here>;
+
+SELECT name FROM lists WHERE
+listID=listRel.listID AND
+listRel.userID=<insert_user_ID_here>;
+
+SELECT val FROM items WHERE
+listID=
