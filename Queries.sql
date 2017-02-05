7eb83bebf94bb260cac0e471c8a4cf417a34d50f
elliotsymons13 <elliotsymons13@gmail.com>
2017-02-05 16:10:52 +0000

    Added the Queries used in the lab to Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -7,4 +7,17 @@ listID=listRel.listID AND
 listRel.userID=<insert_user_ID_here>;
 
 SELECT val FROM items WHERE
-listID=
+items.listID=<insert_list_ID_here> AND
+checked=false;
+
+SELECT val FROM items WHERE
+items.listID=<insert_list_ID_here> AND
+checked=true;
+
+SELECT COUNT(itemID) FROM items WHERE
+items.listID = <insert_list_ID_here>;
+
+SELECT COUNT(itemID) FROM items WHERE
+items.itemID + lsits.listID AND
+lists.listID = listRel.listID AND
+listRel.userID = <insert_user_ID_here>;
