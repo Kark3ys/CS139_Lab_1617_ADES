d3f65ced8b44024fedfab0d3241666c8519956ec
Kark3ys <adam.j.dodson@gmail.com>
2017-02-08 12:36:19 +0000

    Reconfigure Database Class
    Downloaded database.php from the lab page to replace db.php, as well as
    updating folder.php to reference this new file.

--- folder.php
+++ folder.php
@@ -2,7 +2,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set(“display_errors”, 1);
-	require 'db.php';
+	require 'databse.php';
 	$db = new Database();
 	echo '<table id="lists">';
 	$result = $db->query("SELECT lists.listID FROM lists 
