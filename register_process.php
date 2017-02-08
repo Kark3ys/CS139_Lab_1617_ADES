efadf6745b1196e9a3fc1b0d322b2db7d91cb145
Kark3ys <adam.j.dodson@gmail.com>
2017-02-08 14:31:51 +0000

    Removed GET parameters from header passes, switched around error display again.

--- register_process.php
+++ register_process.php
@@ -1,23 +1,25 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require 'database.php';
 if(!empty($_POST["username"])) {
 	//Check for duplicate email/username.
 	$db = new Database();
 	if(!empty($db->querySingle("SELECT userID FROM users 
 			WHERE username='".$_POST["username"]."';"))) {
-		header("Location:register.php?err=1");
+		header("Location:register.php");
+		exit();
 	} elseif(!empty($db->querySingle("SELECT userID FROM users 
 			WHERE email='".$_POST["email"]."';"))) {
-		header("Location:register.php?err=2");
+		header("Location:register.php");
+		exit();
 	} else {
 		$salt = sha1("B");
 		$encPass = sha1($salt."--A");
 		$db->exec("INSERT INTO users(username, pass, salt, email) 
 			VALUES(".$_POST["username"].",".$encPass.",".$salt.",".$_POST["email"].");");
 		header("Location:index.php");
+		exit();
 	}
 }
 ?>
